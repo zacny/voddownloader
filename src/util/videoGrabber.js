d77c1c6dccbdb54d963fb37392238ef5b6973eb2
zacny <20001050+zacny@users.noreply.github.com>
2019-05-22 22:03:33 +0200

    videoGrabber - correct processing of templates array

--- src/util/videoGrabber.js
+++ src/util/videoGrabber.js
@@ -1,36 +1,49 @@
 var VideoGrabber = (function(VideoGrabber){
-    var getVideoData = function(url, w){
+    var getVideoData = function(vod, templateIndex){
+        var idn = vod.grabber.idParser();
+        var templates = vod.grabber.urlTemplates;
+        var url = templates[templateIndex].replace(/\$idn/g, idn);
+
+        console.log(url);
         return $.ajax({
-            url: url,
             method: 'GET',
-            dataType: 'json'
+            dataType: 'json',
+            url: url
         });
+    };
+
+    var tryNextUrl = function(vod, templateIndex, w, error){
+        var templates = vod.grabber.urlTemplates;
+        if(templates[templateIndex+1] !== undefined) {
+            VideoGrabber.grabVideoData(vod, templateIndex+1, w);
+        }
+        else {
+            throw error;
+        }
     };
 
     VideoGrabber.grabVideoData = function(vod, templateIndex, w){
         try {
-            var idn = vod.grabber.idParser();
-            var templates = vod.grabber.urlTemplates;
-            var url = templates[templateIndex].replace(/\$idn/g, idn);
             w = (w === undefined) ? window.open(): w;
-            console.log(url);
-            getVideoData(url, w).then(function(data){
+            getVideoData(vod, templateIndex).then(function(data){
                 try {
                     var formatData = vod.grabber.formatParser(data);
                     if(formatData && formatData.formats.length == 0){
-                        throw CONST.api_structure_error;
+                        tryNextUrl(vod, templateIndex, w, CONST.api_structure_error);
                     }
-                    DomTamper.createDocument(formatData, w);
+                    else {
+                        DomTamper.createDocument(formatData, w);
+                    }
                 }
                 catch(e){
                     DomTamper.handleError(e, w);
                 }
             }, function(data){
-                if(templates[templateIndex+1] !== undefined) {
-                    VideoGrabber.grabVideoData(vod, templateIndex+1, w);
+                try {
+                    tryNextUrl(vod, templateIndex, w, CONST.call_error);
                 }
-                else {
-                    DomTamper.handleError(CONST.call_error, w);
+                catch(e){
+                    DomTamper.handleError(e, w);
                 }
             });
         }
