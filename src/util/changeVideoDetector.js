41eeef70fbdf6b29260b890757228faa57e7dbd3
zacny <fabian.zawicki@gmail.com>
2019-05-22 15:30:44 +0200

    small bug after code refactoring

--- src/util/changeVideoDetector.js
+++ src/util/changeVideoDetector.js
@@ -2,16 +2,18 @@ var ChangeVideoDetector = (function(ChangeVideoDetector){
     var checkVideoChange = function(oldSrc, videoChangeCallback) {
         var src = window.location.href;
         if(src !== undefined && oldSrc !== src){
+            console.log("checkVideoChange: " + oldSrc + " -> " + src);
             return Promise.resolve().then(videoChangeCallback);
         }
         else {
             return Promise.resolve().then(
-                setTimeout(checkVideoChange, Const.attempt_timeout, oldSrc, videoChangeCallback)
+                setTimeout(checkVideoChange, CONST.attempt_timeout, oldSrc, videoChangeCallback)
             );
         }
     };
 
     ChangeVideoDetector.run = function(videoChangeCallback){
+        console.log('ChanageVideoDetector start');
         var src = window.location.href;
         checkVideoChange(src, videoChangeCallback);
     };
